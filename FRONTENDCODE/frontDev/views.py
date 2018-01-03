390f1e206b584667c729b171ccf4733fc2e9a3ab
Jee Kang <jwk1151@terpmail.umd.edu>
2018-01-03 14:02:02 -0500

    login and stuff

--- FRONTENDCODE/frontDev/views.py
+++ FRONTENDCODE/frontDev/views.py
@@ -6,9 +6,11 @@ def admin_side(request):
     return render(request, 'admin_view.html')
 def kiosk(request):
 	template = loader.get_template('kiosk.html')
-	context = {'status' : "offline"}
+	context = {'status' : "online"}
 	return HttpResponse(template.render(context, request))
 def port(request):
 	template = loader.get_template('port.html')
-	context = {'status' : "offline"}
+	context = {'status' : "online"}
 	return HttpResponse(template.render(context, request))
+def login(request):
+	return render(request, 'login.html')
